12587d616040a2b6d3f77e619993eaddeef0c1a8
Denis <denis.rovdo@yandex.com>
2023-03-15 19:20:06 +0100

    feat: add config

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import stule from './App.module.scss';
+
+function App() {
+  return (
+    <div>
+      <li className={stule.list}>2</li>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </div>
+  );
+}
+
+export default App;
